b53af31c921995e5a83c872cc5b1bce6eaebd44e
Debjit Saha <debjitsaha606@gmail.com>
2025-07-26 13:12:14 +0530

    app initialize

--- src/com/VaultX/controller/App.java
+++ src/com/VaultX/controller/App.java
@@ -1,5 +1,17 @@
 package com.VaultX.controller;
 
-public class App {
-	
+import javax.swing.*;
+
+public class App extends JFrame {
+    public static void main(String[] args) {
+        int appWidth = 500;
+        int appHeight = 500;
+        JFrame frame = new JFrame("VaultX");
+        frame.setSize(appWidth,appHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
 }
